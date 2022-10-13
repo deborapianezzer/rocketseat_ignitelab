ae824063da88c96c7e83ead67b499561219217f5
deborapianezzer <deboradpianezzer@gmail.com>
2022-10-12 21:02:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function App() {
+  
+
+  return (
+    <h1>Hello Word</h1>    
+  )
+}
+
+
